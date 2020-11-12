92768e3e2681bbbf2be8e73cdc38b913e7a5a1f7
abbasTrax <abbasjaffary33@gmail.com>
2020-11-12 17:02:22 +0430

    hide rating bar

--- lib/widgets/FiveRating.dart
+++ lib/widgets/FiveRating.dart
@@ -15,10 +15,10 @@ class MyFiveRating extends StatelessWidget {
       itemCount: 5,
       itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
       //If itemBuilder gets error check this issue: https://github.com/sarbagyastha/flutter_rating_bar/issues/44
-      itemBuilder: (context, _) => Icon(
-        Icons.star,
-        color: middlePurple,
-      ),
+      // itemBuilder: (context, _) => Icon(
+      //   Icons.star,
+      //   color: middlePurple,
+      // ),
       onRatingUpdate: (rating) {
         print(rating);
       },
